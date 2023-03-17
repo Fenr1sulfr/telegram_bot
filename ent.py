ca27bccb65d6d966ec4b741afd40b22a2a0cb9fc
perFecto <61329340+perfectoman@users.noreply.github.com>
2023-03-17 23:25:22 +0600

    Add files via upload

---
+++ ent.py
@@ -0,0 +1,47 @@
+ent = '''ЕНТ 
+
+Проходные баллы в AITU:
+
+80 БАЛЛОВ для групп образовательных программ:
+➡️В057 Информационные технологии (Software Engineering, Computer Science, Big Data Analysis, Media Technologies, Mathematical and Computational science)
+➡️В058 Информационная безопасность (Cyber Security) 
+
+70 БАЛЛОВ для групп образовательных программ:
+➡️В059 Коммуникации и коммуникационные технологии (Smart Technologies)
+➡️В063 Электротехника и автоматизация (Industrial Internet of Things)
+➡️B044 Менеджмент и управление (IT Entrepreneurship, IT Management, Digital public administration and services)
+➡️B042 Журналистика и репортерское дело (Digital Journalism)
+
+
+Баллы на грант
+
+Порогового балла для поступления на грант не бывает. Для поступления в аиту вам нужно набрать проходные баллы в наш университет (указанные выше), а также не менее 5 баллов по каждому предмету на ЕНТ и сдать вступительные экзамены AET. А гранты будут распределяться по баллам. В первую очередь гранты получают абитуриенты с наивысшими результатами.
+
+Неизвестно каким будет проходной балл на грант в этом году. 
+
+Но у вас есть возможность оценить свои шансы на получение гранта ориентируясь на прошлые проходные баллы:'''
+
+
+
+
+
+ent_2020 = '''➡️Минимальные баллы на грант за 2020:
+B057 IT (Computer Science, Software Engineering, Big Data Analysis, Industrial Automation, Media Technologies) — 92
+B058 Cyber Security — 89
+B059 Tel.Com.Systems — 85
+B044 IT Management — 110 
+B042 Digital Journalism — 98'''
+
+ent_2021 = '''➡️Минимальные баллы на грант за 2021:
+B057 IT (Computer Science, Software Engineering, Big Data Analysis, Industrial Automation, Media Technologies) — 106 
+B058 Cyber Security — 81
+B059 Tel.Com.Systems — 80 
+B044 IT Management — 120 
+B042 Digital Journalism — 123'''
+
+ent_2022 = '''➡️Минимальные баллы на грант за 2022:
+B057 IT (Software Engineering, Computer Science, Big Data Analysis, Media Technologies, Mathematical and Computational Science) - 104 
+B058 Cyber Security  - 69
+B059 Smart Technologies - 58 
+B044  IT Management, IT Entrepreneurship, Digital Public Administration and Services - 120 
+B042 Digital Journalism - 122'''
---
+++ infos.py
@@ -0,0 +1,77 @@
+import aiogram.utils.markdown as md
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+from ops import *
+from faq import *
+from aet import *
+from ent import *
+
+
+global_kb = types.InlineKeyboardMarkup()
+global_kb_back = types.InlineKeyboardButton(text="Назад", callback_data="back")
+global_kb_back_op = types.InlineKeyboardButton(text="Назад", callback_data="OP")
+global_kb_back_gop = types.InlineKeyboardButton(text="Назад", callback_data="GOP")
+global_kb_back_faq = types.InlineKeyboardButton(text="Назад", callback_data='faq')
+global_kb_back_aet = types.InlineKeyboardButton(text="Назад", callback_data="aet")
+global_kb_back_ent = types.InlineKeyboardButton(text="Назад", callback_data="ent")
+
+
+info = {
+    'aet': f'{aet}',
+    'ent': f'{ent}',
+    'zhata': f'{zhata}'
+}
+
+aet_info = {
+    'aet_money': f'{aet_money}',
+    'aet_certificate': f'{aet_certificate}',
+    'aet_material': f'{aet_material}'
+}
+
+gop = {
+    'it': f'{soft}\n\n{media}\n\n{math}\n\n{comp}\n\n{bigdata}',
+    'cs': f'{cyber}',
+    'com': f'{smart}',
+    'iiot': f'{iiot}',
+    'management': f'{itm}\n\n{ite}\n\n{dpas}',
+    'dj': f'{dj}'
+}
+
+ops = {
+    'SE': f'{soft}',
+    'MT': f'{media}',
+    'CS': f'{cyber}',
+    'IT': f'{comp}',
+    'BDA': f'{bigdata}',
+    'MCS': f'{math}',
+    'ST': f'{smart}',
+    'IIOT': f'{iiot}',
+    'ITM': f'{itm}',
+    'ITE': f'{ite}',
+    'DPAS': f'{dpas}',
+    'DJ': f'{dj}'
+}
+
+ent_info = {
+    'ent_2020': f'{ent_2020}',
+    'ent_2021': f'{ent_2021}',
+    'ent_2022': f'{ent_2022}'
+}
+
+
+
+
+
+# @dp.callback_query_handler(lambda call: call.data in ['SE', 'MT', 'IT', 'MCS', 'BDA', 'CS'])
+# async def opshki(call: types.CallbackQuery):
+#     keyboard = InlineKeyboardMarkup()
+#     keyboard.add(global_kb_back_op)
+#     key = call.data
+#     await call.message.edit_text(f"{ops[key]}", reply_markup=keyboard, parse_mode = 'HTML')
